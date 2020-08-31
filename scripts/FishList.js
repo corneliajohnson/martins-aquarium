05cc7800f1521593b7a7bc8c6d49070e993c77d8
Cornelia Johnson <corneliaj91@gmail.com>
2020-08-31 15:58:19 -0400

    changed FishList funtion to map

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -3,29 +3,27 @@
  */
 
 // TODO: Import `useFish` from the data provider module
-import { Fish } from './Fish.js'
-import { useFish, makeMostHolyFish, makeSoldierFish, makeNonHolyFish } from './FishDataProvider.js'
-const contentElement = document.querySelector(".fishList")
+import { Fish } from "./Fish.js";
+import {
+  useFish,
+  makeMostHolyFish,
+  makeSoldierFish,
+  makeNonHolyFish,
+} from "./FishDataProvider.js";
+const contentElement = document.querySelector(".fishList");
 
 export const FishList = () => {
   // Get a reference to the `<article class="content">` element
   // const fishes = useFish()
-  const holyFish = makeMostHolyFish()
-  const soliderFish = makeSoldierFish()
-  const nonholyfish = makeNonHolyFish()
+  const holyFish = makeMostHolyFish();
+  const soliderFish = makeSoldierFish();
+  const nonholyfish = makeNonHolyFish();
 
-  return whichFish(holyFish)
-}
+  return whichFish(holyFish);
+};
 
 const whichFish = (fishArr) => {
-  //Function done with class
-  let fishHTMLRepresentation = ""
-  for (const fish of fishArr) {
-    fishHTMLRepresentation += Fish(fish);
-  }
-
-  // Add to the existing HTML in the content element
   contentElement.innerHTML += `
-      ${fishHTMLRepresentation}
+      ${fishArr.map((fish) => Fish(fish)).join("")}
     `;
-}
+};
