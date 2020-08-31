0b52d9bc648a6a14b59999001c3da2dbccef0d00
Cornelia Johnson <corneliaj91@gmail.com>
2020-08-31 16:06:50 -0400

    changed LocationList funtion to map

--- scripts/locations/LocationList.js
+++ scripts/locations/LocationList.js
@@ -1,15 +1,10 @@
-import { singleLocation } from './Location.js'
-import { locationArr } from './LocationDataProvider.js'
-
+import { singleLocation } from "./Location.js";
+import { locationArr } from "./LocationDataProvider.js";
 
 let locationsUL = document.querySelector(".locationList");
 
-let liforHTML = "";
-
-locationArr.forEach(place => {
-  liforHTML += singleLocation(place)
-});
-
 export const LocationList = () => {
-  locationsUL.innerHTML = liforHTML;
-}
+  locationsUL.innerHTML = `${locationArr
+    .map((location) => singleLocation(location))
+    .join("")}`;
+};
